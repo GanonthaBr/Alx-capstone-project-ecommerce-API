a3f7813bc0acef86b2896fae8ae7cec6b6c6bd0c
Ganontha <bganontha@gmail.com>
2024-12-19 12:07:00 +0100

    Added the registration feature and authentications with django authtoken

---
+++ mycosmecticnatural/accounts/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username','password','email']
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id','username','email', 'followers','bio','profile_picture','following')
+        read_only_fields = [
+            'id','followers','following'
+        ]
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        print(user)
+        raise serializers.ValidationError('Wrong credentials')
+    
