1b18775fde5927bc09ec6af07783c296d8bf6923
Ganontha <bganontha@gmail.com>
2024-12-27 13:39:49 +0100

    Update cart URLs to include remove functionality and refactor cart details endpoint

--- mycosmecticnatural/products/urls.py
+++ mycosmecticnatural/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.views import CategoriesViewSet, ProductsViewSet, AddToCart
+from products.views import CategoriesViewSet, ProductsViewSet, AddToCart, RemoveFromCart, CartDetails
 from . import views
 
 router = DefaultRouter()
@@ -13,6 +13,7 @@ urlpatterns = [
     path('wishlist/', views.list_wishlist_products, name='wishlist'),
     path('wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
-    path('cart/', views.list_cart_products, name='cart'),
-    path('cart/<int:product_id>/', AddToCart.as_view(), name='add_to_cart'),
+    path('cart/', CartDetails.as_view(), name='cart_items'),
+    path('cart/add/<int:product_id>/', AddToCart.as_view(), name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', RemoveFromCart.as_view(), name='remove_from_cart'),
 ]
