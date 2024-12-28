15ac58e8bfd0a11f305eb31b5f080369f42e238a
Ganontha <bganontha@gmail.com>
2024-12-28 15:02:43 +0100

    update wishlist URL endpoint

--- mycosmecticnatural/products/urls.py
+++ mycosmecticnatural/products/urls.py
@@ -11,7 +11,7 @@ router.register(r'products',ProductsViewSet, basename='products')
 urlpatterns = [
     path('', include(router.urls)),
     path('wishlist/', views.list_wishlist_products, name='wishlist'),
-    path('wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
     path('cart/', CartDetails.as_view(), name='cart_items'),
     path('cart/add/<int:product_id>/', AddToCart.as_view(), name='add_to_cart'),
