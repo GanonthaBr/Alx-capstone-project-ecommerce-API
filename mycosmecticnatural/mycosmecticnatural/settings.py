52a7b52532dca57f540e07808f28fcb30a0a15ff
Ganontha <bganontha@gmail.com>
2025-01-06 13:14:56 +0100

    configure to allow localhost calls

--- mycosmecticnatural/mycosmecticnatural/settings.py
+++ mycosmecticnatural/mycosmecticnatural/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts',
     'products',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mycosmecticnatural.urls'
