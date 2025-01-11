0eb2277270920761c08270003cf1fbd5025fdeb0
Ganontha <bganontha@gmail.com>
2025-01-11 12:12:57 +0100

    Enable CORS middleware in settings

--- mycosmecticnatural/mycosmecticnatural/settings.py
+++ mycosmecticnatural/mycosmecticnatural/settings.py
@@ -42,18 +42,18 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts',
     'products',
-    # 'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mycosmecticnatural.urls'
